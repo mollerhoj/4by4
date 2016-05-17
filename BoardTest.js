fd9135b33edf85d0222443c86b52545cdde20277
Jens Møllerhøj <mollerhoj@Jenss-MacBook-Pro-2.local>
2016-05-17 13:40:07 +0200

    board can return spaces

--- BoardTest.js
+++ BoardTest.js
@@ -46,6 +46,48 @@ module.exports = class BoardTest extends Test {
     this.assertEqual(clone.render(),"..\n.X\n");
   }
 
+  canReturnAllHorizontalSpaces() {
+    let board = new Board();
+    board.buildEmpty(6,7);
+    this.assertEqual(board.horizontalSpaces().length, 24);
+  }
+
+  canReturnAllSpacesInARow() {
+    let board = new Board();
+    board.buildEmpty(6,7);
+    this.assertEqual(board.rowSpaces(0).length, 4);
+  }
+
+  canReturnAllVerticalSpaces() {
+    let board = new Board();
+    board.buildEmpty(6,7);
+    this.assertEqual(board.verticalSpaces().length, 21);
+  }
+
+  canReturnAllSpacesInAColumn() {
+    let board = new Board();
+    board.buildEmpty(6,7);
+    this.assertEqual(board.columnSpaces(0).length, 3);
+  }
+
+  canReturnAllDiagonalDownSpaces() {
+    let board = new Board();
+    board.buildEmpty(6,7);
+    this.assertEqual(board.diagonalDownSpaces().length, 12);
+  }
+
+  canReturnAllDiagonalUpSpaces() {
+    let board = new Board();
+    board.buildEmpty(6,7);
+    this.assertEqual(board.diagonalUpSpaces().length, 12);
+  }
+
+  canReturnAllSpaces() {
+    let board = new Board();
+    board.buildEmpty(6,7);
+    this.assertEqual(board.spaces().length, 69);
+  }
+
   test() {
     this.canBeBuiltEmpty();
     this.canReturnHeight();
@@ -53,5 +95,12 @@ module.exports = class BoardTest extends Test {
     this.canDropToken();
     this.canReturnColumnFull();
     this.canClone();
+    this.canReturnAllSpacesInARow();
+    this.canReturnAllHorizontalSpaces();
+    this.canReturnAllSpacesInAColumn();
+    this.canReturnAllVerticalSpaces();
+    this.canReturnAllDiagonalDownSpaces();
+    this.canReturnAllDiagonalUpSpaces();
+    this.canReturnAllSpaces();
   }
 }
--- board.js
+++ board.js
@@ -1,7 +1,7 @@
 module.exports = class Board {
 
   constructor() {
-    let matrix = null;
+    this.matrix = null;
   }
 
   dropToken(token, x) {
@@ -22,6 +22,15 @@ module.exports = class Board {
     }
     return true;
   };
+
+  isFull() {
+    for(let x = 0; x < this.width(); x++) {
+      if (!this.columnFull(x)) {
+        return false;
+      }
+    }
+    return true;
+  }
 
   clone() {
     let clone = new Board();
@@ -76,5 +85,66 @@ module.exports = class Board {
       str += "\n"
     }
     return str;
+  }
+
+  spaces() {
+    let spaces = [];
+    spaces = spaces.concat(this.horizontalSpaces());
+    spaces = spaces.concat(this.verticalSpaces());
+    spaces = spaces.concat(this.diagonalDownSpaces());
+    spaces = spaces.concat(this.diagonalUpSpaces());
+    return spaces;
+  }
+
+  horizontalSpaces() {
+    let spaces = []
+    for(let y = 0; y < this.height(); y++) {
+      spaces = spaces.concat(this.rowSpaces(y));
+    };
+    return spaces;
+  }
+
+  rowSpaces(y) {
+    let spaces = [];
+    for(let x = 0; x < this.width() - 3; x++) {
+      spaces.push([this.matrix[y][x], this.matrix[y][x+1], this.matrix[y][x+2], this.matrix[y][x+3]]);
+    }
+    return spaces;
+  }
+
+  diagonalDownSpaces() {
+    let spaces = [];
+    for(let y = 0; y < this.height() - 3; y++) {
+      for(let x = 0; x < this.width() - 3; x++) {
+        spaces.push([this.matrix[y][x], this.matrix[y+1][x+1], this.matrix[y+2][x+2], this.matrix[y+3][x+3]]);
+      }
+    }
+    return spaces;
+  }
+
+  diagonalUpSpaces() {
+    let spaces = [];
+    for(let y = 3; y < this.height(); y++) {
+      for(let x = 0; x < this.width() - 3; x++) {
+        spaces.push([this.matrix[y][x], this.matrix[y-1][x+1], this.matrix[y-2][x+2], this.matrix[y-3][x+3]]);
+      }
+    }
+    return spaces;
+  }
+
+  verticalSpaces() {
+    let spaces = []
+    for(let x = 0; x < this.width(); x++) {
+      spaces = spaces.concat(this.columnSpaces(x));
+    };
+    return spaces;
+  }
+
+  columnSpaces(x) {
+    let spaces = []
+    for(let y = 0; y < this.height() - 3; y++) {
+      spaces.push([this.matrix[y][x], this.matrix[y+1][x], this.matrix[y+2][x], this.matrix[y+3][x]]);
+    }
+    return spaces;
   }
 }
