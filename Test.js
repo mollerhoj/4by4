4dfc531e714c0a518dca4ffa7208ac855d41657e
Jens Møllerhøj <mollerhoj@Jenss-MacBook-Pro-2.local>
2016-05-17 13:40:35 +0200

    testing returns stacktrace

--- Test.js
+++ Test.js
@@ -1,6 +1,8 @@
 module.exports = class Test {
   assertEqual(a, b) {
     if (a != b) {
+      var stack = new Error().stack;
+      console.log( stack );
       throw 'test faild, ' + a + ' not equal to ' + b;
     }
   }
